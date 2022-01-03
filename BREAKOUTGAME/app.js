859f3a981a95feffba4cf4f6b697083cc6e187e7
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-03 11:10:13 +0100

    moving the canvas

--- BREAKOUTGAME/app.js
+++ BREAKOUTGAME/app.js
@@ -3,21 +3,44 @@
 
 const canvas = document.querySelector("#canvas")
 const ctx = canvas.getContext('2d')
-// ctx.moveTo(0,0)
-// ctx.lineTo(250,50)
-ctx.beginPath()
-ctx.rect(10,10,50,50)
 
-ctx.fillStyle = "orange"
-ctx.fill()
-ctx.stroke()
-ctx.closePath()
+let x = 100
+let y = 100
+const dx = 5 
+const dy = 3
 
 ctx.beginPath()
-ctx.arc(100,100,30,0,2 * Math.PI, false)
+ctx.arc(x,y,30,0,2 * Math.PI, false)
 ctx.fillStyle = "green"
 ctx.fill()
 ctx.stroke()
 ctx.closePath()
+// drawCirlce(x,y);
+
+setInterval(()=>{
+  x += dx
+  y += dy 
+  // x += 75
+  // y += 50 
+  // if(x >= 900 || y < 600){
+    // x -= 50
+    // y+= 25
+  // }else if(x >=900 || y >= 600){
+    // x -=50
+    // y-= 50
+  // }
+  ctx.clearRect(0,0,canvas.width , canvas.height)
+  drawCirlce(x,y)
+  
+},100)
+
+const drawCirlce = (x,y)=>{
+  ctx.beginPath()
+  ctx.arc(x,y,30,0,2 * Math.PI, false)
+  ctx.fillStyle = "green"
+  ctx.fill()
+  ctx.stroke()
+  ctx.closePath()
+}
 
 // console.log(canvas);
